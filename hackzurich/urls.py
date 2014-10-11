e0017fe30df5aa06e893742dbe96dde33447406b
Bogdan Ciobanu <bogdan.ciobanu@hootsuite.com>
2014-10-11 22:24:54 +0200

    Include urls

--- hackzurich/urls.py
+++ hackzurich/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from api.resources.receipt_resource import ReceiptResource
+
+
+receipt_resource_api = ReceiptResource()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hackzurich.views.home', name='home'),
@@ -8,3 +13,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('hackzurich.api.build_user_data',
+                        url(r'^api/1.0/(?P<client_id>.*)/build_data', 'build_user_data'))
+urlpatterns += patterns('',
+                        (r'^api/', include(receipt_resource_api.urls)),
+                         )
